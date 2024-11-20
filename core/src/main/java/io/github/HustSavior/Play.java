9bf4ab64b0787216b96205718989c9049a4a1342
KhoiNM05 <ng.mh.khoi@gmail.com>
2024-11-20 20:30:16 +0700

    fix collisions

--- core/src/main/java/io/github/HustSavior/Play.java
+++ core/src/main/java/io/github/HustSavior/Play.java
@@ -1,100 +1,150 @@
 package io.github.HustSavior;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import io.github.HustSavior.entities.Player;
-import net.dermetfan.gdx.physics.box2d.PositionController;
+import io.github.HustSavior.utils.CollisionListener;
 
 public class Play implements Screen {
-    private OrthographicCamera camera;
-    private TiledMap map;
-    private OrthogonalTiledMapRenderer renderer;
-    private Player player;
-    private InputHandler inputHandler;
+    private static final float INITIAL_ZOOM = -1.2f;
+    private static final float ZOOM_SPEED = 0.02f;
+    private static final float MIN_ZOOM = 0.1f;
+    private static final float MAX_ZOOM = 10f;
+    private static final float WORLD_STEP_TIME = 1/60f;
+    
+    private final OrthographicCamera camera;
+    private final TiledMap map;
+    private final OrthogonalTiledMapRenderer renderer;
+    private final Player player;
+    private final InputHandler inputHandler;
+    private final World world;
+
+    public Play() {
+        map = new TmxMapLoader().load("map/map.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        camera = setupCamera();
+        world = setupWorld();
+        player = new Player(new Sprite(new Texture("sprites/WalkRight1.png")), 
+                          500, 500, world);
+        inputHandler = new InputHandler(player);
+        createCollisionBodies();
+    }
+
+    private OrthographicCamera setupCamera() {
+        OrthographicCamera cam = new OrthographicCamera();
+        cam.zoom = INITIAL_ZOOM;
+        cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        return cam;
+    }
+
+    private World setupWorld() {
+        World world = new World(new Vector2(0, 0), true);
+        world.setContactListener(new CollisionListener());
+        return world;
+    }
+
+    private void createCollisionBodies() {
+        for (MapObject object : map.getLayers().get("collisions").getObjects()) {
+            if (object instanceof RectangleMapObject) {
+                createStaticBody((RectangleMapObject) object);
+            }
+        }
+    }
+
+    private void createStaticBody(RectangleMapObject rectangleObject) {
+        Rectangle rect = rectangleObject.getRectangle();
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.position.set(rect.x + rect.width / 2, rect.y + rect.height / 2);
+
+        Body body = world.createBody(bodyDef);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(rect.width / 2, rect.height / 2);
+
+        body.createFixture(shape, 0.0f);
+        shape.dispose();
+    }
+
     @Override
     public void show() {
-        // Load the tmx file
-        map = new TmxMapLoader().load("map/map.tmx");
-
-        //Initialize the map renderer with the loaded map
-        renderer = new OrthogonalTiledMapRenderer(map);
-        // Initialize new camera (orthographic)
-        camera = new OrthographicCamera();
-        camera.zoom = -1.2f;
-        // Set the camera to the size of the screen
-        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        player = new Player(new Sprite(new Texture("sprites/WalkRight1.png")), 500,500);
-        inputHandler = new InputHandler(player);
         Gdx.input.setInputProcessor(inputHandler);
     }
+
     @Override
-    public void resize(int width, int height){
-        camera.viewportHeight = height;
-        camera.viewportWidth = width;
-        camera.update();
+    public void render(float delta) {
+        clearScreen();
+        updateGame(delta);
+        drawGame();
+        world.step(WORLD_STEP_TIME, 6, 2);
     }
-    @Override
-    public void render(float delta){
-        // Clear screen
+
+    private void clearScreen() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        // Update the player's position based on input
-        inputHandler.update(delta);
-        // Make the camera follow the player
-        camera.position.set(player.getX() + player.getWidth()/2, player.getY() + player.getHeight()/2, 0);
-        handleZoom();
-        camera.update();
-        //Update and applying camera
-        renderer.setView(camera);
-        // Render the map
-        renderer.render();
-        renderer.getBatch().begin();
-        player.draw(renderer.getBatch());
-        renderer.getBatch().end();
-
-
     }
 
-    //handling zoom in and zoom out
-    private void handleZoom(){
-        if(Gdx.input.isKeyPressed(Input.Keys.PLUS) || Gdx.input.isKeyPressed(Input.Keys.EQUALS)){
-            camera.zoom -= 0.02;
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.MINUS)){
-            camera.zoom += 0.02;
-        }
+    private void updateGame(float delta) {
+        inputHandler.update(delta);
+        updateCamera();
+    }
 
-        camera.zoom = Math.max(0.1f, Math.min(camera.zoom,10f));
+    private void updateCamera() {
+        camera.position.set(
+            player.getX() + player.getWidth()/2,
+            player.getY() + player.getHeight()/2,
+            0
+        );
+        handleZoom();
+        camera.update();
+    }
+
+    private void drawGame() {
+        renderer.setView(camera);
+        renderer.render();
+        renderer.getBatch().begin();
+        player.draw((SpriteBatch)renderer.getBatch());
+        renderer.getBatch().end();
+    }
+
+    private void handleZoom() {
+        if (Gdx.input.isKeyPressed(Input.Keys.PLUS)) {
+            camera.zoom -= ZOOM_SPEED;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.MINUS)) {
+            camera.zoom += ZOOM_SPEED;
+        }
+        camera.zoom = Math.max(MIN_ZOOM, Math.min(camera.zoom, MAX_ZOOM));
     }
 
     @Override
-    public void dispose(){
-        // Dispose to free resources
+    public void dispose() {
         map.dispose();
         renderer.dispose();
         player.getTexture().dispose();
+        world.dispose();
     }
 
-    @Override
-    public void hide(){
-        // Dispose to free resources
-        dispose();
+    @Override public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
     }
 
-    @Override
-    public void pause(){
-        // Pause the game
-    }
-
-    @Override
-    public void resume(){
-
-    }
+    @Override public void hide() { dispose(); }
+    @Override public void pause() {}
+    @Override public void resume() {}
 }
