7aebf70f309eb69d1ddf8f320b4451469131a208
KhoiNM1501 <Khoi.NM235123@sis.hust.edu.vn>
2024-12-06 15:23:07 +0700

    add dialog

---
+++ core/src/main/java/com/badlogic/gdx/scenes/scene2d/ui/WindowStyle.java
@@ -0,0 +1,11 @@
+package com.badlogic.gdx.scenes.scene2d.ui;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class WindowStyle {
+
+}
